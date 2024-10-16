f51d0f00a1bbef4d6a2e6c1baf4b04bddb0664e3
Beta7UltimateGD <viethoang082013principal@gmail.com>
2024-10-16 21:55:31 +0700

    Update test2.co.ob.py

---
+++ test2.co.ob.py
@@ -0,0 +1,23 @@
+def move
+import move =
+[{
+    def EASING
+    def EASE_IN_OUT
+    groupid: ('1')
+import EASING - (-1)
+(-1) import EASE_IN_OUT
+import MOVE_TIME - (1)
+[{
+(1): 0.5/0.50
+import lock (-2)
+[{
+(-2) lock_x = player(2); camera(3); target_mode(4); direction_mode(5)}-id1
+       2.1 + 2; 3.1 + 3; 4.1 + 4; 5.1 + 5
+       id1 + id2
+       lock_y = player(2.1); camera(3.1); small_step(4.1); dynamic_mode(5.1)}-id2
+    id2 = disabled
+    id1 = disabled
+[{
+move_x: 20 = id4
+id4 + id3
+move_y: 20 = id3
